88cd577e8d30afd61b7ea73a7d9e88ea7addb979
Charles Miles Orlando <charles.miles.orlando@ibm.com>
2023-01-30 22:15:01 -0500

    remove card layout

--- src/Layouts/CardLayout/index.js
+++
@@ -1,5 +0,0 @@
-import CardLayout from './CardLayout';
-
-export { CardLayout };
-
-export default CardLayout;
--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -9,7 +9,6 @@ import PageHeaderSkeleton from "./skeleton";
 // Translations
 // import translations from './translations';
 //
-
 // Children
 import ActionsPanel from "./children/ActionsPanel";
 import Breadcrumbs from "./children/Breadcrumbs";
