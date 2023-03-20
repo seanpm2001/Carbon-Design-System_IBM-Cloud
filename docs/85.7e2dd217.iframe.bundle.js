defecb30c0f1539069afc9056f726294ddea5e0f
CharlieOrlando <cmo1991@gmail.com>
2023-03-20 18:41:17 +0000

    Deploying to gh-pages from @ carbon-design-system/ibm-cloud@5696fa96d042992b4c19975c02e58770e9b10c60 🚀

---
+++ docs/85.7e2dd217.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknew_pal=self.webpackChunknew_pal||[]).push([[85],{"./src/components/OrderSummaryV2/_docs-styles.scss":function(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__),__webpack_exports__.default={}}}]);
