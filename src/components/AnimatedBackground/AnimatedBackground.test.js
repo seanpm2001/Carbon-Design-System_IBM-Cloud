a26d013a2da22e318aedefb0c8e3719ec535f621
Maximilian Hammer <Maximilian.Hammer@ibm.com>
2023-03-24 13:53:21 +0100

    test(AnimatedBackground): add unit tests

--- src/components/AnimatedBackground/AnimatedBackground.test.js
+++ src/components/AnimatedBackground/AnimatedBackground.test.js
@@ -1,5 +1,7 @@
-import '@testing-library/jest-dom'
-
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom';
 import React from 'react';
 import { render } from '@testing-library/react';
 import AnimatedBackground from './AnimatedBackground';
@@ -9,5 +11,17 @@ describe('AnimatedBackground', () => {
     const { container } = render(<AnimatedBackground />);
     const AnimatedBackgroundClass = container.querySelector('.pal--animated-background');
     expect(AnimatedBackgroundClass).toBeInTheDocument();
+
+    const svgClass = container.querySelector('.pal--animated-background__gradient--motion');
+    expect(svgClass).toBeInTheDocument();
+  });
+
+  it('renders', () => {
+    const { container } = render(<AnimatedBackground motion={false} />);
+    const AnimatedBackgroundClass = container.querySelector('.pal--animated-background');
+    expect(AnimatedBackgroundClass).toBeInTheDocument();
+
+    const svgClass = container.querySelector('.pal--animated-background__gradient--motion');
+    expect(svgClass).not.toBeInTheDocument();
   });
 });
