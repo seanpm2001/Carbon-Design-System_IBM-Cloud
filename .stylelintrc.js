2062dfd60a8022bedad313f33b5bbf3082bc135a
Maximilian Hammer <Maximilian.Hammer@ibm.com>
2023-04-14 15:21:40 +0200

    build(stylelint): migrate stylelint rules

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["stylelint-config-standard-scss"],
+  "rules": {
+    "at-rule-no-unknown": null,
+    "max-nesting-depth": 3,
+    "scss/at-rule-no-unknown": true,
+    "no-descending-specificity": null,
+    "selector-class-pattern": null,
+    "keyframes-name-pattern": null,
+    "string-quotes": null,
+    "media-feature-range-notation": "prefix"
+  }
+}
