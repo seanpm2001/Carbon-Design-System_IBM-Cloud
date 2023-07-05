8790478a5bdbd938caf1fdfdfe24f929ec8a74dc
Stephanie Cree <stephanie.cree@ibm.com>
2023-07-05 17:17:45 -0500

    fix(AnimatedNumbers): adding basic skeleton

---
+++ src/components/BigNumbers/skeleton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Skeleton = () => {
+  return <div className="pal--big-numbers__skeleton>">
+          <div className="pal--big-numbers__numbers">loading...</div>
+      </div>;
+};
+
+export default Skeleton
